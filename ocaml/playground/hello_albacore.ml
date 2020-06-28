3d491c31b54b3389836d14855a1dd25ad1b5c596
brandlk <kathi.brandl@gmx.de>
2020-06-28 13:37:06 +0200

    working lcu parser

--- ocaml/playground/hello_albacore.ml
+++ ocaml/playground/hello_albacore.ml
@@ -45,6 +45,12 @@ module Lcu =
       | Var of string
       | Lambda of string * t
       | App of t * t
+
+    let rec to_string (e: t): string =
+      match e with
+      | Var x -> x
+      | Lambda (x,e1) -> "(" ^ "\\" ^ x ^ "." ^ (to_string e1) ^ ")"
+      | App (e1,e2) -> "(" ^ (to_string e1) ^ " " ^ (to_string e2) ^ ")"
   end
 
 module Make (Final: ANY) =
@@ -65,30 +71,48 @@ module Make (Final: ANY) =
       return (printf "variable in column: %d\n" (Fmlib.Character_parser.Position.column p)) >>= fun _ ->
       var_name >>= fun v -> return (Lcu.Var v)
 
+    let whitespace_char: char t =
+      one_of_chars " \n\t" "whitespace_char"
+
+    let whitespace: unit t =
+      map (fun _ -> ())
+        (skip_zero_or_more whitespace_char)
+
     let rec lambda (): Lcu.t t =
       get_position >>= fun p ->
       return (printf "lambda in column: %d\n" (Fmlib.Character_parser.Position.column p)) >>= fun _ ->
 
       return (fun v e -> Lcu.Lambda (v,e))
       |. (char '\\')
+      |. whitespace
       |= var_name
+      |. whitespace
       |. (char '.')
+      |. whitespace
       |= expression ()
 
-    and app (): Lcu.t t =
-      (* get_position >>= fun p ->
-       * return (printf "app in column: %d\n" (Fmlib.Character_parser.Position.column p)) >>= fun _ -> *)
-
-      expression () >>= fun e1 ->
-      expression () >>= fun e2 ->
-      return (Lcu.App (e1,e2))
     
+    and basic_expression (): Lcu.t t =
+      get_position >>= fun p ->
+      return (printf "basic_expression in column: %d\n" (Fmlib.Character_parser.Position.column p)) >>= fun _ ->
+
+      (return identity
+       |. char '('
+       |= expression ()
+       |. char ')')
+      <|> variable
+      <|> lambda ()
+
     and expression (): Lcu.t t =
       get_position >>= fun p ->
       return (printf "expression in column: %d\n" (Fmlib.Character_parser.Position.column p)) >>= fun _ ->
       
-      variable <|> lambda () <|> app ()
-
+      one_or_more_separated (basic_expression ()) whitespace >>= fun lst ->
+      return (match lst with
+              | [] -> assert false
+              | hd :: tl ->
+                 List.fold_left (fun acc e -> Lcu.App (acc,e)) hd tl) 
+        
     let test_pipeline: string t =
       return (fun _ _ -> "blubb")
       |= var_name
@@ -107,14 +131,21 @@ let test_variable () =
   printf "result: %s\n" r;;
 
 
-let test_lcu () =
+let test_lcu (input: string) =
   let module LcuP = Make (Lcu) in
   let open LcuP in
-  let p = run (expression ()) "\\x.x" in
-  let r = match result p with
-    | Some (Lambda (x, Var y)) when x=y -> "ok"
-    | _ -> "error" in
-  printf "%s\n" r;;
+  let p = run (expression ()) input in
+  match result p with
+  | Some e ->
+     printf "success:\n%s\n\n" (Lcu.to_string e);
+  | _ ->
+     printf "error\n\n";;
 
 
-test_lcu ()
+
+test_lcu "\\x.x";
+test_lcu "x y z";
+test_lcu "\\x . \\y.x";
+test_lcu "(\\x.x)(\\x.x)";
+test_lcu "\\x.x\\x.x"
+
